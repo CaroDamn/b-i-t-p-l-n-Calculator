7f0feb8765a1d33ab6e6b798a19fcf4a1a54924d
CaroDuong <copthoht@gmail.com>
2026-05-12 19:58:25 +0700

    cap nhật fraction .h

--- BTL1_Entity/include/Fraction.h
+++ BTL1_Entity/include/Fraction.h
@@ -5,10 +5,23 @@
 
 class Fraction {
 private:
-
+    long long num = 0;
+    long long den = 1;
+    bool sign = 0;
 
 public:
-
+    Fraction(long long num, long long den, bool sign);
+    Fraction(string input);
+    void simplify();
+    string toString();
+    Fraction operator+(Fraction other);
+    Fraction operator-(Fraction other);
+    Fraction operator*(Fraction other);
+    Fraction operator/(Fraction other);
+    bool operator==(Fraction other);
+    bool operator=(Fraction other);
+    bool operator>(Fraction other);
+    bool operator<(Fraction other);
 };
 
 #endif
