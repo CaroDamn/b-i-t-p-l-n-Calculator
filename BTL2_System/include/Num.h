6faf8efd45a6da79e3b771c49bd430414d5aefba
CaroDamn <copthoht@gmail.com>
2026-06-16 19:24:27 +0700

    16/6/2026

--- BTL2_System/include/Num.h
+++ BTL2_System/include/Num.h
@@ -1,5 +1,6 @@
 #ifndef NUM_H
 #define NUM_H
+#include "../../BTL1_Entity/include/Fraction.h"
 
 #include <string>
 
@@ -9,5 +10,17 @@ public:
     virtual Num* add(Num* other) = 0;
     virtual std::string toString() const = 0;
 };
+class NumF : public Num {
+private:
+    Fraction num;
+public:
+    NumF(Fraction f){
+        this -> num = f;
+    }
+    Num* add(Num* other) {
+        return this -> num + other -> num;
+    }
 
+
+}
 #endif
